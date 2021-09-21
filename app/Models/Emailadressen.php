c822049669f501efeff5bc1e35eaf2f43b67fca3
calu <johan.calu@gmail.com>
2021-09-21 16:17:39 +0200

    model emailadressen aangemaakt

---
+++ app/Models/Emailadressen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Emailadressen extends Model
+{
+    use HasFactory;
+}
